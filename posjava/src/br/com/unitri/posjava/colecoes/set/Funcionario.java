478441b59fc79f901c1381c8b2a613313a567ce7
Emilio Carvalho Dias <emiliodias@MacBook-Pro-de-Emilio.local>
2017-09-09 16:29:37 -0300

    sets

---
+++ posjava/src/br/com/unitri/posjava/colecoes/set/Funcionario.java
@@ -0,0 +1,62 @@
+package br.com.unitri.posjava.colecoes.set;
+
+public class Funcionario {
+	
+	private String nome;
+	
+	private Long id;
+	
+	public Funcionario(String nome, Long id) {
+		
+		this.nome = nome;
+		this.id = id;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [nome=" + nome + ", id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
