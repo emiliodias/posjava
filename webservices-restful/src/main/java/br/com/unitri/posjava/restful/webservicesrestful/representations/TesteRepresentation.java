625502a1a8d4e79f9dd18317f378f55c8609c08f
Emilio Carvalho Dias <emiliodias@MacBook-Pro-de-Emilio.local>
2018-05-13 11:13:05 -0300

    exemplos

---
+++ webservices-restful/src/main/java/br/com/unitri/posjava/restful/webservicesrestful/representations/TesteRepresentation.java
@@ -0,0 +1,21 @@
+package br.com.unitri.posjava.restful.webservicesrestful.representations;
+
+public class TesteRepresentation {
+	
+	private String name;
+	
+	public TesteRepresentation(String name) {
+		
+		this.name = name;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
